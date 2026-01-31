fd0ea6df8d57e2579eaddf0c1486b036758591ad
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-01-31 18:44:19 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 START_TIME=$(date +%s)
 
-echo "script executed at:: $START_TIME"
+echo "script executed at: $START_TIME"
 
 sleep 10
 
 END_TIME=$(date +%s)
 
-echo "script executed at:: $END_TIME"
-
 
 TOTAl_TIME=$(($END_TIME-$START_TIME))
 
-echo "script executed in:: $TOTAL_TIME" seconds
+echo "script executed in: $TOTAL_TIME seconds"
