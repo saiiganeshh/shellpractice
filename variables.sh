e7b9af9cb2e5fb5d5868e938fc24b36ff65c3c35
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-01-31 17:43:43 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "saiganesh:: Hey, Hi sarada what are u doing"
+echo "sarada:: Hey, Hi saiganesh im doing good how abt you"
+echo "saiganesh:: Thanks sarada for asking. What are u learning"
+echo "sarada:: I am learning devops saiganesh.
