7eb5468d3d2ae5d368840f08b6dc89bebd060093
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-01-31 18:03:54 +0530

    username

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:: Hey, Hi $person2 what are u doing"
+echo "$person2:: Hey, Hi $person1 im doing good how abt you"
+echo "$person1:: Thanks $person2 for asking. What are u learning"
+echo "$person2:: I am learning devops $person1"
