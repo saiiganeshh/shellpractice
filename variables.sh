9b3bb2c036737715a68068e657c781ef40ed0457
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-01-31 17:49:58 +0530

    variable1

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-person1=saiganesh
-person2=sarada
+person1=modi
+person2=trump
 echo "$person1:: Hey, Hi $person2 what are u doing"
 echo "$person2:: Hey, Hi $person1 im doing good how abt you"
 echo "$person1:: Thanks $person2 for asking. What are u learning"
