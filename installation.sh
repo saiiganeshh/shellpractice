4786b3bef3c9cad0a4be3ea16b44061d6635b850
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-02-05 17:33:32 +0530

    installation

--- installation.sh
+++ installation.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+userid=$( id -u)
 
-USERID=$(id -u)
+if [ $userid -ne 0 ]; then
 
-# Root user check
-if [ $USERID -ne 0 ]; then
-    echo "Run as root user"
-    exit 1
+echo "run as root user"
+
+exit 1
+
 fi
 
-echo "Installing nginx..."
+echo "installing nginx"
+
 dnf install nginx -y
 
-# Check installation status
 if [ $? -ne 0 ]; then
-    echo "Installing nginx... FAILURE"
-    exit 1
+
+echo "Nginx installtion .... FAILURE"
+exit 1
+
 else
-    echo "Installing nginx... SUCCESS"
+
+echo "Nginx installion ...Success"
+
 fi
