51b970615a8e33abef18d3f59b6c8d153f6035af
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-02-05 20:04:04 +0530

    loops

--- 15loops.sh
+++ 15loops.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
+#!/bin/bash
+
 user=$(id -u)
 
-if [ $user -ne 0 ]; then
-
-echo " run as root user"
-exit 1
+if [ "$user" -ne 0 ]; then
+  echo "Run as root user"
+  exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then 
+VALIDATE() {
+  if [ "$1" -ne 0 ]; then
     echo "$2 .... FAILURE"
-
     exit 1
-
-    else
+  else
     echo "$2 .... SUCCESS"
-    fi
+  fi
 }
-
 
 for package in "$@"
 do
-dnf list installed $package
-if [ $? -ne 0 ]; then 
-echo " $package is not instahhed installing now"
-dnf install $package -y
-VALIDATE $? "installing $package"
-
-else
-echo "$package already installed skipping now"
-fi
+  dnf list installed "$package" &>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "$package is not installed, installing now"
+    dnf install "$package" -y
+    VALIDATE $? "installing $package"
+  else
+    echo "$package already installed, skipping now"
+  fi
 done
+
